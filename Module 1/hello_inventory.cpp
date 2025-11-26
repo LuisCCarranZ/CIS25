96afbe8671ee56565466cc751045c033769eb494
LCarranza <carranzacamposluis@gmail.com>
2025-11-25 21:27:04 -0800

    Add files via upload

---
+++ Module 1/hello_inventory.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	//Program prints to the screen different Data Types
+	cout << "Hello Inventory System!\n";
+	cout << "\tData Types:\n";
+	cout << "Size of int: " << sizeof(int) << " bytes\n";
+	cout << "Size of float: " << sizeof(float) << " bytes\n";
+	cout << "Size of double: " << sizeof(double) << " bytes\n";
+	cout << "Size of char: " << sizeof(char) << " bytes\n";
+	cout << "Size of bool: " << sizeof(bool) << " bytes\n";
+	cout << "Size of long: " << sizeof(long) << " bytes\n";
+	cout << "Size of long long: " << sizeof(long long) << " Bytes\n";
+	return 0;
+}
